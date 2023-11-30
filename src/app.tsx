14210f33b04ab66530cd46e9c62238d1e981ebef
Håkon Matland <hakon.matland@autodesk.com>
2023-11-30 11:40:59 +0100

    Print Bobs offer

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,11 @@
+import WebRtcChat from "./components/WebRtcChat";
+
 export default function App() {
   return (
     <>
       <h1>Multiplayer</h1>
       <p>Hello world!</p>
+      <WebRtcChat />
     </>
   );
 }
